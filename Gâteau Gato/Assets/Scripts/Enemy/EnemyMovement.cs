a856dad51701d33d970e00b1cd37b653d8fa2012
GrayAidan <114709817+GrayAidan@users.noreply.github.com>
2024-05-09 16:55:03 -0400

    Player Controller and AI scripts added

---
+++ Gâteau Gato/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    
+    private GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
