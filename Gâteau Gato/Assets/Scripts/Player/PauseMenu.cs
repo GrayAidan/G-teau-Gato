015c39d41272667192bcf5fac6a3950cee1059df
GrayAidan <114709817+GrayAidan@users.noreply.github.com>
2024-05-10 12:09:07 -0400

    Pause Menu and Main Menu

--- Gâteau Gato/Assets/Scripts/Player/PauseMenu.cs
+++ Gâteau Gato/Assets/Scripts/Player/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -19,6 +20,11 @@ public class PauseMenu : MonoBehaviour
         {
             PauseGame();
         }
+    }
+
+    public void ExitGame()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void PauseGame()
