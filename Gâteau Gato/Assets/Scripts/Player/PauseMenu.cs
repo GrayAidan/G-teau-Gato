5d1c8a2311c8e6677b672c29719971122baabad8
GrayAidan <114709817+GrayAidan@users.noreply.github.com>
2024-05-10 11:15:58 -0400

    PauseMenu

---
+++ Gâteau Gato/Assets/Scripts/Player/PauseMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+        else
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
